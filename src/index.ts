bf8908cdda0d68598d114dfe71e912bb3f2451d5
Anneli Savchenko <foranneli@gmail.com>
2022-12-23 14:18:06 +0100

    fix: fix styles in main-section.scss, products-section.scss, modal-window.scss, main.scss

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,32 @@ import './components/main-section/aside/dual-slider/dual-slider';
 import './components/main-section/aside/aside';
 import './components/main-section/main-section';
 import './components/main-page/header/header';
+// import './components/modal-window-page/modal-window-page';
 
 import { header, createHeader } from './components/main-page/header/header';
 import { footer, createFooter } from './components/main-page/footer/footer';
 // import { createDetailsPage } from './components/details-page/details';
 import { createProductsSection } from './components/main-section/products-section/products-section';
-// import { createPriceDualSlider } from './components/main-section/aside/dual-slider/dual-slider';
+import { createPriceDualSlider } from './components/main-section/aside/dual-slider/dual-slider';
 // import { createFilterSubСategories, createFilterСategories } from './components/main-section/aside/filter/filter';
 import { createAside } from './components/main-section/aside/aside'; 
 import { productsCartBlock, summaryCartBlock } from './components/cart-page/cart-page';
-// createHeader(header);
-// createFooter(footer);
+import { createProducstPage } from './components/main-section/main-section';
+
+
+
+createHeader(header);
+createFooter(footer);
+
+const mainSection = document.querySelector('.main') as HTMLElement;
 
 // mainSection?.append(createDetailsPage(3));
 // mainSection.append(createProductsSection());
 // mainSection.append(createPriceDualSlider());
 // mainSection.append(createFilterSubСategories());
 // mainSection.append(createFilterСategories());
-// mainSection.append(createAside(), createProductsSection());
+mainSection.append(createProducstPage());
 // createProductsSection();
 //-----------------------MODULE WINDOW--------------------------------//
-import './components/modal-window-page/modal-window-page';
+
 //--------------------------------------------------------------------//
