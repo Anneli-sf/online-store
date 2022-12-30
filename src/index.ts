3855e20ccba2141e412abb160b7e4e97c40cb2ee
Anneli Savchenko <foranneli@gmail.com>
2022-12-31 00:10:41 +0100

    refactor: implement choosing of one category with routing

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,20 @@ createHeader();
 createFooter();
 
 const mainSection = document.querySelector('.main') as HTMLElement;
-mainSection.append(createProducstPage(productsData));
+// mainSection.append(createProducstPage(productsData));
 
 //---------------------------ROUTE------------------------//
 
 const MainPage = {
     render: () => {
-        // mainSection.innerHTML = '';
         return createProducstPage(productsData);
+    },
+};
+
+const MainPageWithFilters = {
+    render: (currentArray) => {
+        return createProducstPage(currentArray);
+        // console.log(`${currentArray} works`);
     },
 };
 
@@ -56,130 +62,156 @@ const routes = [
     { path: '/cart', component: CartPage },
 ];
 
-// idArray.forEach((item) => {
-//     routes.push({ path: `#/product-details/${item}`, component: DetailsPage });
-// });
-
 if (!localStorage.getItem('cartList')) {
     localStorage.setItem('cartList', JSON.stringify([]));
 }
 if (!localStorage.getItem('cartItems')) {
     localStorage.setItem('cartItems', JSON.stringify([]));
 }
+
+//-------------------------------ROUTING
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+const findComponentByPath = (path, routes) => {
+    console.log('routes', routes);
+    console.log('path', path);
+    return routes.find((r) => r.path.match(new RegExp(`^\\${path}$`, 'gmi'))) || undefined;
+};
+
+const router = (option) => {
+    const path = parseLocation();
+    console.log('path parse', path);
+    const { component = ErrorComponent } = findComponentByPath(path, routes) || {};
+
+    mainSection.innerHTML = ``;
+    mainSection.append(component.render(option));
+};
+
+window.addEventListener('hashchange', () => router());
+window.addEventListener('load', () => router());
+//-------------------------------/ROUTING
+
+//-------variables for filters
 let currDataWithCategories: IProductsData[] = [];
 let currDataWithSubCategories: IProductsData[] = [];
 let stackArr: IProductsData[] = [];
+let currentArray: IProductsData[] = []; 
 
 // TODO   SAVE THE PAGE when RELOAD
 document.addEventListener('click', (e: Event) => {
-    // console.log(e);
-
     //---------if click on DETAILS
     if (e.target instanceof Element && e.target.parentElement && e.target.closest('.btn__details')) {
         const state: string = '#/product-details/' + e.target.id;
         window.history.pushState({ path: state }, '', state);
 
-        const link = e.target.parentElement as HTMLLinkElement;
-        link.href = `#${window.location.href.split('#')[1]}`;
-        // console.log('link.href', link.href);
+        // const link = e.target.parentElement as HTMLLinkElement;
+        // link.url = `#${window.location.href.split('#')[1]}`;
+        e.target.url = window.location.href;
 
         routes.push({ path: window.location.href.split('#')[1], component: DetailsPage });
         console.log('routes', routes);
-        // router(Number(e.target.id));
+        router(Number(e.target.id));
     }
 
-    //---------if click on FILTERS CATEGORY
+    //-------------------------------------------------------
+
     if (e.target instanceof Element && e.target.className === 'category-label') {
-        //e.target.classList[0] === 'category-label'
-        // if (e.target.tagName == 'LABEL') {
-        //     e.target.classList.add('checked');
-        // } else {
-        //     e.target.classList.add('checked');
-        // }//--------ничего не добавляет
+        console.log('e.target', e.target);
+        const categoryName: string = e.target.getAttribute('for'); //sport
+        console.log('categoryName', categoryName);
+        const state: string = '#/category=' + categoryName; //работает без ?
+        window.history.pushState({ path: state }, '', state);
+        e.target.url = window.location.href;
+        console.log('e.target.url', e.target.url);
 
-        const chosenCategoryArr: IProductsData[] = productsData.filter((item) => {
-            const element = e.target as HTMLLabelElement;
-            if (element.children[0] !== null) return item.categoryEng === element.children[0].id;
-        });
-        
-        console.log('chosenCategory', chosenCategoryArr);
-        // if (!isAlreadyHave(, chosenCategoryArr)) {
-        //      = .concat(chosenCategoryArr);
-        //     localStorage.setItem('productsList', JSON.stringify());
-        // } else {
-        //      = deleteChosenCategory(, chosenCategoryArr);
-        //     if (.length === 0) {
-        //         .concat(productsData);
-        //     }
-        //     localStorage.setItem('productsList', JSON.stringify());
-        // }
-
-        currDataWithCategories = deleteDoubleAddUnique(currDataWithCategories, chosenCategoryArr);
-        console.log('');
-        // localStorage.setItem('productsList', JSON.stringify());
-
-        if (currDataWithCategories.length === 0) {
-            mainSection.innerHTML = ``;
-            mainSection.append(createProducstPage(productsData));
-        } else {
-            mainSection.innerHTML = ``;
-            mainSection.append(createProducstPage(currDataWithCategories));
-        }
+        routes.push({ path: window.location.href.split('#')[1], component: MainPageWithFilters });
+        console.log('routes', routes);
+        const chosenCategory = productsData.filter((el) => el.categoryEng === categoryName);
+        currentArray = deleteDoubleAddUnique(currentArray, chosenCategory);
+        console.log('Arr для вызова', currentArray);
+        router(currentArray);
     }
+    //---------if click on FILTERS CATEGORY
+    // if (e.target instanceof Element && e.target.className === 'category-label') {
+    //     const chosenCategoryArr: IProductsData[] = productsData.filter((item) => {
+    //         const element = e.target as HTMLLabelElement;
+    //         if (element.children[0] !== null) return item.categoryEng === element.children[0].id;
+    //     });
 
-    //---------if click on FILTERS SUBCATEGORY
-    if (e.target instanceof Element && e.target.className === 'subcategory-label') {
-    
-    //если категории уже выбраны
-        if (currDataWithCategories.length > 0) {
-            const chosenSubCategoryArr: IProductsData[] = currDataWithCategories.filter((item) => {
-                const element = e.target as HTMLLabelElement;
-                if (element.children[0] !== null) return item.subcategoryEng === element.children[0].id;
-            });
-            console.log('chosenSubCategory', chosenSubCategoryArr);
+    //     console.log('chosenCategory', chosenCategoryArr);
+    //     // if (!isAlreadyHave(, chosenCategoryArr)) {
+    //     //      = .concat(chosenCategoryArr);
+    //     //     localStorage.setItem('productsList', JSON.stringify());
+    //     // } else {
+    //     //      = deleteChosenCategory(, chosenCategoryArr);
+    //     //     if (.length === 0) {
+    //     //         .concat(productsData);
+    //     //     }
+    //     //     localStorage.setItem('productsList', JSON.stringify());
+    //     // }
+    //     currDataWithCategories = deleteDoubleAddUnique(currDataWithCategories, chosenCategoryArr);
+    //     // localStorage.setItem('productsList', JSON.stringify());
 
-            if (!isAlreadyHave(stackArr, chosenSubCategoryArr)) {
-                stackArr = stackArr.concat(chosenSubCategoryArr);
-                currDataWithSubCategories = currDataWithSubCategories.concat(stackArr);
-                // console.log('currDataWithSubCategories', currDataWithSubCategories);
-            } else if (isAlreadyHave(stackArr, chosenSubCategoryArr)) {
-                stackArr = deleteDoubleAddUnique(stackArr, chosenSubCategoryArr); //удаляем кликнутое второй раз
-                // console.log('stackArr второй клик', stackArr);
-                if (stackArr.length === 0) {
-                    currDataWithSubCategories = currDataWithCategories;
-                    // console.log('currDataWithSubCategories когда стек пустой', currDataWithSubCategories);
-                } else {
-                    currDataWithSubCategories = addDoubleDeleteUnique(currDataWithSubCategories, stackArr);
-                }
-            }
+    //     if (currDataWithCategories.length === 0) {
+    //         mainSection.innerHTML = ``;
+    //         mainSection.append(createProducstPage(productsData));
+    //     } else {
+    //         mainSection.innerHTML = ``;
+    //         mainSection.append(createProducstPage(currDataWithCategories));
+    //     }
+    // }
 
-            mainSection.innerHTML = ``;
-            mainSection.append(createProducstPage(currDataWithSubCategories));
-        } else if (currDataWithCategories.length === 0) {
-            //если категории НЕ выбраны
-            const chosenSubCategoryArr: IProductsData[] = productsData.filter((item) => {
-                const element = e.target as HTMLLabelElement;
-                if (element.children[0] !== null) return item.subcategoryEng === element.children[0].id;
-            });
-            console.log('chosenSubCategory при пустых категориях', chosenSubCategoryArr);
+    // //---------if click on FILTERS SUBCATEGORY
+    // if (e.target instanceof Element && e.target.className === 'subcategory-label') {
+    //     //если категории уже выбраны
+    //     if (currDataWithCategories.length > 0) {
+    //         const chosenSubCategoryArr: IProductsData[] = currDataWithCategories.filter((item) => {
+    //             const element = e.target as HTMLLabelElement;
+    //             if (element.children[0] !== null) return item.subcategoryEng === element.children[0].id;
+    //         });
+    //         console.log('chosenSubCategory', chosenSubCategoryArr);
 
-            if (!isAlreadyHave(stackArr, chosenSubCategoryArr)) {
-                stackArr = stackArr.concat(chosenSubCategoryArr);
-                currDataWithSubCategories = stackArr;
-                console.log('currDataWithSubCategories при пустом стеке', currDataWithSubCategories);
-            } else if (isAlreadyHave(stackArr, chosenSubCategoryArr)) {
-                stackArr = deleteDoubleAddUnique(stackArr, chosenSubCategoryArr);
+    //         if (!isAlreadyHave(stackArr, chosenSubCategoryArr)) {
+    //             stackArr = stackArr.concat(chosenSubCategoryArr);
+    //             currDataWithSubCategories = currDataWithSubCategories.concat(stackArr);
+    //             // console.log('currDataWithSubCategories', currDataWithSubCategories);
+    //         } else if (isAlreadyHave(stackArr, chosenSubCategoryArr)) {
+    //             stackArr = deleteDoubleAddUnique(stackArr, chosenSubCategoryArr); //удаляем кликнутое второй раз
+    //             // console.log('stackArr второй клик', stackArr);
+    //             if (stackArr.length === 0) {
+    //                 currDataWithSubCategories = currDataWithCategories;
+    //                 // console.log('currDataWithSubCategories когда стек пустой', currDataWithSubCategories);
+    //             } else {
+    //                 currDataWithSubCategories = addDoubleDeleteUnique(currDataWithSubCategories, stackArr);
+    //             }
+    //         }
 
-                if (stackArr.length > 0) {
-                    currDataWithSubCategories = addDoubleDeleteUnique(currDataWithSubCategories, stackArr);
-                } else if (stackArr.length === 0) {
-                    currDataWithSubCategories = productsData;
-                }
-            }
-            mainSection.innerHTML = ``;
-            mainSection.append(createProducstPage(currDataWithSubCategories));
-        }
-    }
+    //         mainSection.innerHTML = ``;
+    //         mainSection.append(createProducstPage(currDataWithSubCategories));
+    //     } else if (currDataWithCategories.length === 0) {
+    //         //если категории НЕ выбраны
+    //         const chosenSubCategoryArr: IProductsData[] = productsData.filter((item) => {
+    //             const element = e.target as HTMLLabelElement;
+    //             if (element.children[0] !== null) return item.subcategoryEng === element.children[0].id;
+    //         });
+    //         console.log('chosenSubCategory при пустых категориях', chosenSubCategoryArr);
+
+    //         if (!isAlreadyHave(stackArr, chosenSubCategoryArr)) {
+    //             stackArr = stackArr.concat(chosenSubCategoryArr);
+    //             currDataWithSubCategories = stackArr;
+    //             console.log('currDataWithSubCategories при пустом стеке', currDataWithSubCategories);
+    //         } else if (isAlreadyHave(stackArr, chosenSubCategoryArr)) {
+    //             stackArr = deleteDoubleAddUnique(stackArr, chosenSubCategoryArr);
+
+    //             if (stackArr.length > 0) {
+    //                 currDataWithSubCategories = addDoubleDeleteUnique(currDataWithSubCategories, stackArr);
+    //             } else if (stackArr.length === 0) {
+    //                 currDataWithSubCategories = productsData;
+    //             }
+    //         }
+    //         mainSection.innerHTML = ``;
+    //         mainSection.append(createProducstPage(currDataWithSubCategories));
+    //     }
+    // }
 
     //-------------------BASKET
     if (e.target instanceof Element && e.target.parentElement && e.target.closest('.btn__add')) {
@@ -280,21 +312,6 @@ document.addEventListener('click', (e: Event) => {
         }
     }
 });
-
-const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
-const findComponentByPath = (path, routes) =>
-    routes.find((r) => r.path.match(new RegExp(`^\\${path}$`, 'gmi'))) || undefined;
-
-const router = (id?: number) => {
-    const path = parseLocation();
-    const { component = ErrorComponent } = findComponentByPath(path, routes) || {};
-
-    mainSection.innerHTML = ``;
-    mainSection.append(component.render(id));
-};
-
-window.addEventListener('hashchange', () => router());
-window.addEventListener('load', () => router());
 
 // //--------------------------------------------------------//
 
