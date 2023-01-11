6cd81bcc8433b4a456b4559652b6a8f7c212b6d2
Maestro <akievaleksandr@mail.ru>
2023-01-11 23:09:17 +0500

    refactor: delete error page

--- src/components/error-page/error-page.ts
+++
@@ -1,7 +0,0 @@
-import './error-page.scss';
-import { createElement } from '../global-components/global-components';
-
-export const createErrorPage = () => {
-    const block = createElement('div', 'error-block') as HTMLDivElement;
-    return block;
-};
